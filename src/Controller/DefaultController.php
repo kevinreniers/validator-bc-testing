d1402c9d0ae6ab32c3eddbafd6a693a5065b2cd5
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2020-03-02 00:44:31 +0100

    Add another example for lists with inner resources.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -10,11 +11,35 @@ class DefaultController extends AbstractController
     /**
      * @Route("/default", name="default")
      */
-    public function index()
+    public function index(): Response
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/DefaultController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/all", name="all")
+     */
+    public function all(): Response
+    {
+        $items = [
+            [
+                'first_name' => 'Joe',
+                'last_name' => 'Star',
+                'date_of_birth' => (new \DateTime('1970-01-01'))->format('Y-m-d')
+            ],
+            [
+                'first_name' => 'Foo',
+                'last_name' => 'Bar',
+                'date_of_birth' => (new \DateTime('1975-04-01'))->format('Y-m-d')
+            ],
+        ];
+
+        return $this->json([
+            'items' => $items,
+            'count' => \count($items)
         ]);
     }
 }
--- tests/DefaultControllerTest.php
+++ tests/DefaultControllerTest.php
@@ -4,13 +4,18 @@ namespace App\Tests;
 
 use App\Util\Tester;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Validator\Constraints\All;
 use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\Validator\Constraints\Count;
+use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use function App\Util\assertMatchesConstraints;
+use Symfony\Component\Validator\Constraints\Positive;
+use Symfony\Component\Validator\Constraints\Required;
+use Symfony\Component\Validator\Constraints\Type;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testIndexHasExpectedResponse(): void
     {
         $client = $this->createClient();
         $client->request('GET', '/default');
@@ -19,6 +24,33 @@ class DefaultControllerTest extends WebTestCase
             'fields' => [
                 'message' => new NotBlank(),
                 'path' => new NotBlank(),
+            ]
+        ]);
+
+        Tester::assertJsonMatchesConstraints($client->getResponse()->getContent(), $constraints);
+    }
+
+    public function testAllHasExpectedResponse(): void
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/all');
+
+        $constraints = new Collection([
+            'fields' => [
+                'items' => new Required([
+                    new Type('array'),
+                    new Count(['min' => 1]),
+                    new All([
+                        new Collection([
+                            'fields' => [
+                                'first_name' => new NotBlank(),
+                                'last_name' => new NotBlank(),
+                                'date_of_birth' => new Date(),
+                            ]
+                        ])
+                    ]),
+                ]),
+                'count' => new Required([new Type('int'), new Positive()]),
             ]
         ]);
 
